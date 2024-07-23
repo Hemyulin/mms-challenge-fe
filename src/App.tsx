a2c8f71e9f61505a502f18dadaaef7ba9565eb32
Hemyulin <daniel.budiansky@protonmail.com>
2024-07-23 15:46:41 +0200

    working on form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    const url = `http://localhost/3000/graphql`;
+
+    const postData = async () => {
+      const data = {};
+    };
+
+    try {
+      const data = 10;
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+    [0];
+  });
+
   return (
     <>
       <h1>LOL</h1>
+      <form></form>
     </>
   );
 }
