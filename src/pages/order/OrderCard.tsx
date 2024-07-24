860ed79d9dbfa3b2b2b772c4e150233b7c6fdca9
Hemyulin <daniel.budiansky@protonmail.com>
2024-07-24 14:15:19 +0200

    adding navbar

--- src/pages/order/OrderCard.tsx
+++ src/pages/order/OrderCard.tsx
@@ -3,8 +3,10 @@ import "./OrderCard.css";
 export const OrderCard = () => {
   return (
     <div className="order-card">
-      <h5>current state: OPEN</h5>
-      <h4>Customer Name</h4>
+      <div className="title">
+        <h4>Long Johnson</h4>
+        <h5 className="status">current state: OPEN</h5>
+      </div>
       <h5>Line Items</h5>
       <ul>
         <li>Screen</li>
@@ -12,10 +14,6 @@ export const OrderCard = () => {
       </ul>
       <p>Created at 27.07.24</p>
       <p>Updated at 28.07.24</p>
-      <p>
-        id: ID! currentState: OrderStatus! customer: Customer! employee:
-        Employee! lineItems: [String!]! createdAt: String! updatedAt: String!
-      </p>
     </div>
   );
 };
